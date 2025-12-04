c609ced3bb626c57509f686176156089a22f67d6
hchtr <155396759+hchtr@users.noreply.github.com>
2025-12-04 18:25:51 +0100

    Update profile.ps1

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,3 +1,9 @@
+#
 Clear-Host
-
-Set-Location -Path "$env:HCHTR_HOME"
+#
+if (Test-Path "$env:HCHTR_HOME") {
+  Set-Location -Path "$env:HCHTR_HOME"
+} else {
+  Write-Host "HCHTR_HOME not set or directory missing." -ForegroundColor Red
+}
+#
