5f51cb044b36a95aca14fac5e16d31624196207f
hchtr <155396759+hchtr@users.noreply.github.com>
2025-12-04 18:23:30 +0100

    Update setup.ps1

--- bin/setup.ps1
+++ bin/setup.ps1
@@ -1,9 +1,8 @@
+# 
 [System.Environment]::SetEnvironmentVariable("HCHTR_HOME", "C:/HCHTR", "User")
-
-$repoProfile = "$env:HCHTR_HOME\repos\dotfiles-win\powershell\profile.ps1"
-
+#
+$REPO_PROFILE = "$env:HCHTR_HOME\repos\dotfiles-win\powershell\profile.ps1"
 New-Item -ItemType Directory -Force -Path (Split-Path $PROFILE) | Out-Null
-
 Remove-Item $PROFILE -Force -ErrorAction Ignore
-
-New-Item -ItemType SymbolicLink -Path $PROFILE -Target $repoProfile
+New-Item -ItemType SymbolicLink -Path $PROFILE -Target $REPO_PROFILE
+#
