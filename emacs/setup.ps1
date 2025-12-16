81b0b02c48611ea115461a8578aaf68aee05832c
hchtr <155396759+hchtr@users.noreply.github.com>
2025-12-16 13:48:02 +0100

    Update setup.ps1

--- emacs/setup.ps1
+++ emacs/setup.ps1
@@ -1,5 +1,5 @@
 #
-$REPO_INIT_EL = "$env:HCHTR_HOME\repos\dotfiles-win\emacs\init.el"
+$REPO_INIT_EL = "$env:HCHTR_HOME\repos\env\emacs\init.el"
 $INIT_EL = "$env:APPDATA\.emacs.d\init.el"
 Remove-Item $INIT_EL -Force -ErrorAction Ignore
 New-Item -ItemType SymbolicLink -Path $INIT_EL -Target $REPO_INIT_EL
