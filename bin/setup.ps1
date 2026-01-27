f6aaa21b37cd0c1d77f22f0ef11f96d3b112ea57
hchtr <155396759+hchtr@users.noreply.github.com>
2026-01-27 20:59:06 +0100

    Delete bin/setup.ps1

--- bin/setup.ps1
+++
@@ -1,8 +0,0 @@
-# 
-[System.Environment]::SetEnvironmentVariable("HCHTR_HOME", "C:/HCHTR", "User")
-#
-$REPO_PROFILE = "$env:HCHTR_HOME\repos\env\powershell\profile.ps1"
-New-Item -ItemType Directory -Force -Path (Split-Path $PROFILE) | Out-Null
-Remove-Item $PROFILE -Force -ErrorAction Ignore
-New-Item -ItemType SymbolicLink -Path $PROFILE -Target $REPO_PROFILE
-#
