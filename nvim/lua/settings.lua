da3a920989439f9361cfc4edc34d5f4e7e40ab19
hchtr <155396759+hchtr@users.noreply.github.com>
2026-01-28 23:05:05 +0100

    Update settings.lua

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,1 +1,21 @@
+local opt = vim.opt
 
+vim.cmd("syntax on")
+vim.cmd("filetype plugin indent on")
+opt.autoread = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.autoindent = true
+opt.expandtab = true
+
+opt.wildmenu = true
+
+opt.number = true
+opt.relativenumber = true
+
+opt.termguicolors = true 
+
+opt.hlsearch = true
+opt.incsearch = true
+opt.smartcase = true
