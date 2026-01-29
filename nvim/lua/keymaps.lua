9cd4992b1db9f21b750fe504d0de9b84df9e4f42
hchtr <155396759+hchtr@users.noreply.github.com>
2026-01-29 17:48:47 +0100

    Update keymaps.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,8 +1,25 @@
+local utils = require("utils")
 --
 vim.api.nvim_set_keymap('i', '(', '()<Esc>i', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '[', '[]<Esc>i', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '{', '{}<Esc>i', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '"', '""<Esc>i', { noremap = true, silent = true })
 --
-vim.api.nvim_set_keymap("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], {expr = true, noremap = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], {expr = true, noremap = true})
+vim.keymap.set("i", "<Tab>", function()
+  if vim.fn.pumvisible() == 1 then
+    return "<C-n>"
+  elseif utils.check_backspace() then
+    return "<Tab>"
+  else
+    return "<C-x><C-o>"
+  end
+end, { expr = true, silent = true, noremap = true })
+
+vim.keymap.set("i", "<S-Tab>", function()
+  if vim.fn.pumvisible() == 1 then
+    return "<C-p>"
+  else
+    return "<C-h>"
+  end
+end, { expr = true, silent = true, noremap = true })
+--
