fd9e6975817a57532db6fe0c6fb163498dbdfb6b
hchtr <155396759+hchtr@users.noreply.github.com>
2026-01-29 13:42:11 +0100

    Update autocmds.lua

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,3 +1,4 @@
+-- 
 vim.api.nvim_create_autocmd("VimEnter", {
     callback = function()
         if os.getenv("HCHTR_HOME") then
@@ -5,3 +6,19 @@ vim.api.nvim_create_autocmd("VimEnter", {
         end
     end,
 })
+--
+vim.api.nvim_create_autocmd({"BufReadPost", "BufNewFile"}, {
+    pattern = { "*.c", "*.h" },
+    callback = function()
+        if vim.fn.executable("clangd") == 1 then
+            vim.lsp.buf_attach_client(0, vim.lsp.start_client({
+                name = "clangd",
+                cmd = { "clangd" },
+                root_dir = vim.loop.cwd(),
+                filetypes = { "c" },
+            }))
+            vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
+        end
+    end,
+})
+--
