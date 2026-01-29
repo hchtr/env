0667f12bc934da02a441fb56dab30251f29e538f
hchtr <155396759+hchtr@users.noreply.github.com>
2026-01-29 13:40:57 +0100

    Update keymaps.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,1 +1,2 @@
-
+vim.api.nvim_set_keymap("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], {expr = true, noremap = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], {expr = true, noremap = true})
