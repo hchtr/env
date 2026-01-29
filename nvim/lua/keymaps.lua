676162ec7a72c0d301a59ed3a52bc5bf754bc413
hchtr <155396759+hchtr@users.noreply.github.com>
2026-01-29 14:38:49 +0100

    Update keymaps.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,2 +1,8 @@
+--
+vim.api.nvim_set_keymap('i', '(', '()<Esc>i', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '[', '[]<Esc>i', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '{', '{}<Esc>i', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '"', '""<Esc>i', { noremap = true, silent = true })
+--
 vim.api.nvim_set_keymap("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], {expr = true, noremap = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], {expr = true, noremap = true})
