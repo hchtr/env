47a3c80b9d1644eba664ee0f06fc4505540a99bb
hchtr <155396759+hchtr@users.noreply.github.com>
2026-06-20 13:48:17 +0100

    Update profile.ps1

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -7,6 +7,7 @@ if (Test-Path "$env:HCHTR_HOME") {
 #
 function ee { explorer . }
 function hm { sl $env:HCHTR_HOME }
+function cdrg { sl -Path "$env:HCHTR_HOME/repos/game" }
 ##
 function conf { cmake --preset $args }
 function build { cmake --build --preset $args }
